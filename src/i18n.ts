56b67e8eeb70032a546f956550930f96d79044a6
安风 <2196477263@qq.com>
2026-05-20 14:47:16 +0800

    fix: 修改德语枚举值

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@
  * @Author: 安风 2196477263@qq.com
  * @Date: 2026-01-30 13:37:15
  * @LastEditors: 安风 2196477263@qq.com
- * @LastEditTime: 2026-05-20 14:43:55
+ * @LastEditTime: 2026-05-20 14:46:29
  * @FilePath: /playSite/src/i18n.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -32,7 +32,7 @@ const resources = {
   French: {
     translation: french,
   },
-  Geran: {
+  German: {
     translation: german,
   },
   Italian: {
