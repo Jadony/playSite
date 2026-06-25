4d3d29b4036ac820d3f8cb95b89c25abaf4d04d8
安风 <2196477263@qq.com>
2026-06-25 21:37:12 +0800

    feat: 添加跳转页面后重置到顶部

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+/*
+ * @Author: 安风 2196477263@qq.com
+ * @Date: 2026-01-24 16:13:31
+ * @LastEditors: 安风 2196477263@qq.com
+ * @LastEditTime: 2026-06-25 21:36:05
+ * @FilePath: /playSite/src/main.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/locale/zh_CN";
 import App from "./App.tsx";
 import "./styles/global.css";
 import "dayjs/locale/zh-cn";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 // 创建 React Query 客户端
 const queryClient = new QueryClient({
@@ -34,6 +52,7 @@ const theme = {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <QueryClientProvider client={queryClient}>
         <ConfigProvider locale={zhCN} theme={theme}>
           <App />
