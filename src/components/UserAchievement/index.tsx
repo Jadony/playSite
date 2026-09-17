c85b9f1b178e7e4cd1bc69167d9d1083721a84fb
安风 <2196477263@qq.com>
2026-09-17 16:52:24 +0800

    fix: 修改ts报错

--- src/components/UserAchievement/index.tsx
+++ src/components/UserAchievement/index.tsx
@@ -24,6 +24,7 @@ const UserAchievement = () => {
             achievementBody: "",
             invertedImgUrl: "",
             bigImgUrl: "",
+            needImgUrl: "",
             unlockTime: "",
           }),
         );
