be5c30e1c7e02ce481b74b2d3d8a56981a9a03d5
安风 <2196477263@qq.com>
2026-09-20 17:35:52 +0800

    fix: 修改首页成就按钮文案

--- src/components/AchievementModal/index.tsx
+++ src/components/AchievementModal/index.tsx
@@ -2,7 +2,7 @@
  * @Author: 安风 2196477263@qq.com
  * @Date: 2026-03-11 18:09:53
  * @LastEditors: 安风 2196477263@qq.com
- * @LastEditTime: 2026-09-10 21:43:37
+ * @LastEditTime: 2026-09-20 17:35:23
  * @FilePath: /playSite/src/components/AchievementModal/index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -73,7 +73,7 @@ const AchievementModal: React.FC<AchievementModalProps> = ({
             onClick={() => closeRegisterModal(false)}
             className="ml-[104px] w-[350px] max-w-[350px]"
           >
-            立即注册领取
+            Register to claim
           </PrimaryButton>
         </div>
       </div>
